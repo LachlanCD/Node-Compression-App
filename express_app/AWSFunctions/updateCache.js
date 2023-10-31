5e95c42f5933586944222b3d53ffc0c6c99033c2
Lachlan Garrahy <lachikent@gmail.com>
2023-11-01 02:52:15 +1000

    can connect to redis

--- express_app/AWSFunctions/updateCache.js
+++ express_app/AWSFunctions/updateCache.js
@@ -1,39 +1,39 @@
+// Importing required libraries and modules
 const Redis = require('ioredis');
 
-// Initialize the Redis cluster client
-const cluster = new Redis.Cluster([
-  {
-    host: 'group87-redis-cluster.km2jzi.ng.0001.apse2.cache.amazonaws.com',
-    port: 6379
-  }
-]);
+// Your ElastiCache Redis endpoint URL and port
+const redisEndpoint = 'group87-redis-cluster.km2jzi.ng.0001.apse2.cache.amazonaws.com';
+const redisPort = 6379;
 
-// Connection established
-cluster.on('connect', () => {
-  console.log('Connected to Redis Cluster');
+// Initialize Redis client
+// Connection string generally has the format: 'redis://username:password@hostname:port'
+const redis = new Redis({
+  host: redisEndpoint,
+  port: redisPort,
+  // Additional options can be added here
 });
 
-// Connection error
-cluster.on('error', (err) => {
-  console.error(`Error: ${err}`);
+// Adding event listeners to handle events
+
+// Listen for the "connect" event to confirm that the client has connected successfully
+redis.on('connect', () => {
+  console.log('Connected to Redis');
 });
 
-// On client closed
-cluster.on('end', () => {
-  console.error('Client closed');
+// Listen for the "error" event to catch and display errors
+redis.on('error', (error) => {
+  console.error('Redis error', error);
 });
 
-// Test set and get
-cluster.set('key', 'value', (err, reply) => {
-  if (err) {
-    return console.error(`Set Error: ${err}`);
-  }
-  console.log(`Set Reply: ${reply}`);
+// Sample function to set and get a key-value pair
+const testRedis = async () => {
+  // Set key "foo" to hold the value "bar"
+  await redis.set('foo', 'bar');
+  
+  // Retrieve and log the value of key "foo"
+  const value = await redis.get('foo');
+  console.log(`Value of key foo: ${value}`);
+};
 
-  cluster.get('key', (err, reply) => {
-    if (err) {
-      return console.error(`Get Error: ${err}`);
-    }
-    console.log(`Get Reply: ${reply}`);
-  });
-});
+// Execute the sample function
+testRedis().catch(console.error);
