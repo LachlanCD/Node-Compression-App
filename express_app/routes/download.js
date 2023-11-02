8b033a9db61a564312998a9982e859829e5f26f1
Lachlan Garrahy <lachikent@gmail.com>
2023-11-03 03:09:26 +1000

    ff

--- express_app/routes/download.js
+++ express_app/routes/download.js
@@ -10,8 +10,6 @@ router.get('/:key', async (req, res, next) => {
         const fileData = await downloadFromS3(fileKey);
         res.setHeader('Content-Disposition', `attachment; filename=${fileData.fileName}`);
         res.setHeader('Content-Type', fileData.contentType);
-        res.status(200)
-        res.json({ message: `files ${fileKey} successfully downloaded` });
         res.send(fileData.content);
     } catch (err) {
         next(err)
